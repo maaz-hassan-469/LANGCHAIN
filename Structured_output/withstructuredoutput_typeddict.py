a22b5a8f8e429de411d683064fc408b3442ef90e
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-14 12:40:16 -0700

    Update withstructuredoutput_typeddict.py

--- Structured_output/withstructuredoutput_typeddict.py
+++ Structured_output/withstructuredoutput_typeddict.py
@@ -1,13 +1,16 @@
 from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
-from typing import TypedDict
+from typing import TypedDict,Annotated,Optional,Literal
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class review(TypedDict):
 
-    summary:str
-    sentiment:str
+    key_themes:Annotated[list[str],'write down all the themes that are mentioned in the review']
+    summary:Annotated[str,'A brief summary of the review']
+    sentiment:Annotated[Literal["pos","neg"],'return sentiment of the review']
+    pros:Annotated[Optional[list[str]],'write all the pros that are mentioned in the review']
+
 
 
 llm=HuggingFaceEndpoint(repo_id='deepseek-ai/DeepSeek-V4-Pro',
@@ -17,8 +20,9 @@ model=ChatHuggingFace(llm=llm)
 
 structured_model=model.with_structured_output(review)
 
-result=structured_model.invoke("""Disappointed, stopped working after two days
-At first, it seemed fine, but on the second day of use, it just completely stopped turning on. I tried changing the batteries and checking the connection, but nothing worked. I will be returning this for a refund""")
+result=structured_model.invoke("""Right out of the box, the first thing you notice is the premium feel. The matte finish looks fantastic and does a wonderful job of resisting fingerprint smudges, which was a huge issue on the previous generation's model. It has a reassuring weight to it—not so heavy that it becomes tedious to carry around all day, but substantial enough that it doesn't feel like a cheap plastic toy.
+
+The buttons are incredibly tactile and give a satisfying, clicky feedback when pressed. My only minor complaint about the physical design is the placement of the secondary ports. They are located on the bottom left edge, which makes plugging in accessories a bit awkward if you are using it while it charges.""")
 
 print(result)
 print(result['summary'])
