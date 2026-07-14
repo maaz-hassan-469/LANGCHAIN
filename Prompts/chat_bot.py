7af4cde89c1a0af9a14ad3ce068f9454efb084e5
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-14 06:49:17 -0700

    Update chat_bot.py

--- Prompts/chat_bot.py
+++ Prompts/chat_bot.py
@@ -1,4 +1,5 @@
 from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from langchain_core.messages import HumanMessage,SystemMessage,AIMessage
 
 from dotenv import load_dotenv
 
@@ -8,14 +9,16 @@ llm=HuggingFaceEndpoint(repo_id='deepseek-ai/DeepSeek-V4-Flash',
                         task='text-generation')
 model=ChatHuggingFace(llm=llm)
 
-chat_history=[]
+chat_history=[
+    SystemMessage(content="you are a helpful assistant")
+]
 while True:
     user_input=input("you:")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input=="exit":
         break
     result=model.invoke(chat_history)
-    chat_history.append(result)
+    chat_history.append(AIMessage(content=result.content))
     print("AI:",result.content)
 
 print(chat_history)
