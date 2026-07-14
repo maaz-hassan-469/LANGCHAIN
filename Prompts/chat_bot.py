1524e75dc78c8af0c01dfdbb4c1e8d210507bbf3
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-14 02:53:19 -0700

    maintaining chat_history

--- Prompts/chat_bot.py
+++ Prompts/chat_bot.py
@@ -8,10 +8,14 @@ llm=HuggingFaceEndpoint(repo_id='deepseek-ai/DeepSeek-V4-Flash',
                         task='text-generation')
 model=ChatHuggingFace(llm=llm)
 
+chat_history=[]
 while True:
     user_input=input("you:")
+    chat_history.append(user_input)
     if user_input=="exit":
         break
-    result=model.invoke(user_input)
-    print(result.content)
+    result=model.invoke(chat_history)
+    chat_history.append(result)
+    print("AI:",result.content)
 
+print(chat_history)
