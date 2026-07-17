194c7456ee6dd24f909fd2f7a3af478c43ea22b4
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-17 04:37:50 -0700

    Update runnable_branch.py

--- Primitive Runnables/runnable_branch.py
+++ Primitive Runnables/runnable_branch.py
@@ -9,3 +9,22 @@ llm=HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V4-Pro",
                         task="text-generation")
 model=ChatHuggingFace(llm=llm)
 
+prompt1=PromptTemplate(template="create a report on this {topic}",
+                       input_variables=['topic'])
+
+prompt2=PromptTemplate(template="summarize the topic {text}",
+                       input_variables=['text'])
+
+
+parser=StrOutputParser()
+
+topic_chain=RunnableSequence(prompt1,model,parser)
+
+count_chain=RunnableBranch(
+    (lambda x:len(x.split())> 100,RunnableSequence(prompt2,model,parser)),
+    RunnablePassthrough()
+)
+final_chain=topic_chain|count_chain
+
+result=final_chain.invoke({"topic":"war"})
+print(result)
