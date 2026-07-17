8aac305b426e6921bd9b6fc7409788d213d9555c
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-17 02:43:10 -0700

    Create sequence_runnables.py

---
+++ Runnables/sequence_runnables.py
@@ -0,0 +1,21 @@
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableSequence
+from dotenv import load_dotenv
+load_dotenv()
+
+llm=HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V4-Pro",
+                        task="text-generation")
+model=ChatHuggingFace(llm=llm)
+
+prompt1=PromptTemplate(template="give me short detail on this {topic}",
+                       input_variables=['topic'])
+
+parser=StrOutputParser()
+
+chain=RunnableSequence(prompt1,model,parser)
+
+result=chain.invoke({"topic":"runnables"})
+
+print(result)
