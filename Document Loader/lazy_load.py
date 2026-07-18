3349e3537097a47afec79191c62eaed620df4c97
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-18 05:56:20 -0700

    Create lazy_load.py

---
+++ Document Loader/lazy_load.py
@@ -0,0 +1,17 @@
+from langchain_community.document_loaders import DirectoryLoader,PyPDFLoader
+
+loader=DirectoryLoader(
+    path='books',
+    glob='*.pdf',
+    loader_cls=PyPDFLoader
+)
+
+docs=loader.lazy_load()
+
+for document in docs:
+    print(document.page_content)
+
+
+# print(docs[1])
+
+# print(len(docs))
